543b4edd4a427cbb8155195c61462f5c33ca5612
mansinighot93 <mansinighot93@gmail.com>
2024-11-11 13:05:14 +0530

    override toString() function

---
+++ HelloSolution/HelloApp/Product.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloApp
+{
+    public class Product
+    {
+        public string title;
+        public string description;
+        public double unitPrice;
+        public int qunatity;
+        public int id;
+
+        //Read only Property
+        public int ID
+        {
+            get { return this.id;}
+        }
+        public string Title
+        {
+            get { return this.title; }
+            set { this.title = value; }
+        }
+        public string Description
+        {
+            get { return this.description; }
+            set { this.description = value; }
+        }
+        public double UnitPrice
+        {
+            get { return this.unitPrice;}
+            set { this.unitPrice = value;}
+        }
+        public int Qunatity
+        {
+            get { return this.qunatity; }
+            set { this.qunatity = value;}
+        }
+
+        public Product() { 
+
+        }
+        public Product(int id,string title, string description, double unitPrice,int quantity)
+        {
+            this.id = id;
+            this.title = title;
+            this.description = description;
+            this.unitPrice = unitPrice;  
+            this.qunatity= quantity;
+        }
+        ~Product() { 
+        
+        }
+        public override string ToString() { 
+            return this.id + " " + this.title + " " + this.description + " " + this.unitPrice + " " + this.qunatity;
+        }
+    }
+}
--- HelloSolution/HelloApp/Program.cs
+++ HelloSolution/HelloApp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,37 @@ namespace HelloApp
 {
     public class Program
     {
+        //Getter,Setter Method
+        private string programName;
+        public void SetProgram(string name)
+        {
+            this.programName = name;
+        }
+
+        public string GetProgram()
+        {
+            return this.programName;
+        } // Old Version In Write type of this code
+         
+
+        //Property Getter,Setter
+        public string Name
+        {
+            get { return this.programName; }
+            set { this.programName = value; }
+        } //New Version In Write Type of this code but both syntax used in new version
+
+
+        //Constructor OverLoading
+        public Program() {
+            this.programName = "DotNet Learning";
+        }
+        public Program(string name)
+        {
+            this.programName = name;
+        }
+
+
         //Static Function
         static int Addition(int num1,int num2)
         {
@@ -54,12 +86,19 @@ namespace HelloApp
             //Calling Class
             Program program = new Program();
             program.Display();
+            program.Name = "My best program"; //Set Property
+            Console.WriteLine(program.Name); // Get Property
 
             int add = Addition(1, 2);
             int mul = Multiplication(1, 2);
             int sub = Substraction(1, 2);
             int div = Divide(1, 2);
             Console.WriteLine(add + " " + mul + " " + sub + " " + div);
+
+            Product product1 = new Product(1,"Rose","Valentine Flower",4500,20);
+            Console.WriteLine(product1);
+            Product product2 = new Product(2, "Gerbera", "Weeding Flower", 450, 10);
+            Console.WriteLine(product2);
 
             Console.ReadLine();
         }
