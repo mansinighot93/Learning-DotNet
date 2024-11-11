7e727d542296713fbb5abb9fa76ae7789d33babc
mansinighot93 <mansinighot93@gmail.com>
2024-11-11 10:59:01 +0530

    Create Solution File and Console Project in Visual Studio

---
+++ HelloSolution/HelloApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloApp
+{
+    public class Program
+    {
+        static void Main(string[] args) //String array is represent command line argument
+        {
+            Console.WriteLine("Hello World ! " + args[0]);
+            Console.WriteLine("Welcome To DotNet Programming ...");
+            int count = 45;
+            count = count++;
+            if(count <= 300)
+            {
+                while(count < 299)
+                {
+                    Console.WriteLine("Count {0} ",count);
+                    count++;
+                }
+            }
+            Console.WriteLine("Please Enter Your Name :");
+            string name = Console.ReadLine();
+            Console.WriteLine("Good Morning {0} ",name);
+            Console.ReadLine();
+        }
+    }
+}
