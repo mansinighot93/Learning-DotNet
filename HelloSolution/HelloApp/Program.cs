666f65891be031cac7e0a913f11f79ac0025601e
mansinighot93 <mansinighot93@gmail.com>
2024-11-11 12:15:08 +0530

    basic C# learning

--- HelloSolution/HelloApp/Program.cs
+++ HelloSolution/HelloApp/Program.cs
@@ -8,9 +8,34 @@ namespace HelloApp
 {
     public class Program
     {
+        //Static Function
+        static int Addition(int num1,int num2)
+        {
+            return num1 + num2;
+        }
+        static int Multiplication(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+        static int Substraction(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+        static int Divide(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+        //Access Specifier: Public ,Private,Protected.Internal
+        //Member Function
+        public void Display()
+        {
+            Console.WriteLine("Welcome To Dotnet Programming language");
+        }
         static void Main(string[] args) //String array is represent command line argument
         {
-            Console.WriteLine("Hello World ! " + args[0]);
+            for (int i = 0; i < args.Length; i++) {
+                Console.WriteLine("Hello World ! " + args[i]);
+            }
             Console.WriteLine("Welcome To DotNet Programming ...");
             int count = 45;
             count = count++;
@@ -25,6 +50,17 @@ namespace HelloApp
             Console.WriteLine("Please Enter Your Name :");
             string name = Console.ReadLine();
             Console.WriteLine("Good Morning {0} ",name);
+
+            //Calling Class
+            Program program = new Program();
+            program.Display();
+
+            int add = Addition(1, 2);
+            int mul = Multiplication(1, 2);
+            int sub = Substraction(1, 2);
+            int div = Divide(1, 2);
+            Console.WriteLine(add + " " + mul + " " + sub + " " + div);
+
             Console.ReadLine();
         }
     }
