77b5da0a9eed46185e66b6e83c8a9e9a06b71756
mansinighot93 <mansinighot93@gmail.com>
2024-11-14 11:11:42 +0530

    Boxing And Unboxing

--- HelloSolution/HelloApp/BoxingUnboxingTest.cs
+++ HelloSolution/HelloApp/BoxingUnboxingTest.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace HelloApp
 {
-    internal class BoxingUnboxingTest
+    class BoxingUnboxingTest
     {
+        static void Main(string[] args)
+        {
+            //Value type
+            int count = 45;
+
+            //Refernce type:
+            //Boxing is a technique where value type are transformed into refernce type
+            //Value type is wrapped in refernce type
+            Object o = count;
+            Console.WriteLine(o);
+
+            //Unboxing is a technique where refernce type is unwrapped and value type is recived
+            int result = (int)o;
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
     }
 }
