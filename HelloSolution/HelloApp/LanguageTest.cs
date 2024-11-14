647a5fb206be129ab78b02d30653c91225dc2f2e
mansinighot93 <mansinighot93@gmail.com>
2024-11-14 11:04:01 +0530

    OOPs Concept

--- HelloSolution/HelloApp/LanguageTest.cs
+++ HelloSolution/HelloApp/LanguageTest.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,6 +7,37 @@ using System.Threading.Tasks;
 
 namespace HelloApp
 {
+    //Values Types: Values are stored on stack
+    //Refernce Type: They are always stored on heap.
+    //Heap is used for dynamic memory allocation for objects in .NET
+    //Heap is managed by Garbage Collector
+    //Garbage Collector takes care Automatic Memory Management
+    //Abstract class enforce overriding
+    public abstract class Shape
+    {
+        //Minimum one method has to be abstract
+        abstract public void Draw();
+        public string Color { get; set; }
+        public string Width { get; set; }
+        public void Display()
+        {
+            Console.WriteLine("Shape is getting displayed");
+        }
+    }
+    public class Line : Shape
+    {
+        //Member Variables
+        public Point StartPoint;
+        public Point EndPoint;
+        public override void Draw()
+        {
+            Console.WriteLine("Line is drawn..");
+            Console.WriteLine("First Point {0},{1}",StartPoint.x,StartPoint.y);
+            Console.WriteLine("Second Point {0},{1}", EndPoint.x, EndPoint.y);
+            Console.WriteLine("Color = {0}", Color);
+            Console.WriteLine("Width = {0}", Width);
+        }
+    }
     //Proto type :used to define contract,used to define specification,used to define guidlines
     //is used to enforce rules while building concrete class
     //Printing is a skill,a capability
@@ -22,7 +54,9 @@ namespace HelloApp
         void Scan();
     }
     //Proto type is implemented by concrete class
-    //Class Type:Concrete and Abstract Class
+    //Class Type:
+    //Concrete class :classfrom who can create object it is a class from whom instance of class
+    //Abstract Class:will be there but cannot create object of that class
     //Interface is implemeneted an multiple classes
     //Create n no.of interface
     class Printer : IPrintable, IScannable // Multiple interafce inheritance
@@ -81,11 +115,12 @@ namespace HelloApp
             //Values pointed by refernces type are always store on Heap
             //Heap is managed by Garbage Collector
             //Interface are always used polymorphic behaviour and lossely couple architecuture
-           // Printer laserPrinter = new Printer();
+            // Printer laserPrinter = new Printer();
+            Console.WriteLine("Demo For Interface");
             IPrintable outputDevice = null;
             outputDevice = new Printer();
-            //Late Binding:
-            //Early Binding:Function Overloading static polymorphism which you call i.e function overloading
+            //Late Binding:Function is resolve at the runtime
+            //Early Binding:Function is resolve at the Complile time,Function Overloading static polymorphism which you call i.e function overloading
             //When interface is used always is it going to consider late Binding?->yes
             outputDevice.Print();
             outputDevice.Start();
@@ -99,6 +134,21 @@ namespace HelloApp
             outputDevice.Stop();
 
             LanguageTest test = new LanguageTest();
+            Console.WriteLine("Demo for Abstract Class");
+            Shape theShape = new Line();
+            //Type Casting using keyword is used 'as'
+            Line l = theShape as Line;
+            l.StartPoint = new Point();
+            l.StartPoint.x = 56;
+            l.StartPoint.y = 34;
+            l.EndPoint = new Point();
+            l.EndPoint.x = 78;
+            l.EndPoint.y = 56;
+
+            theShape.Color = "Black";
+            theShape.Width = "23";
+            theShape.Draw();
+
             Console.WriteLine("Welcome to C#");
             Console.ReadLine();
         }
