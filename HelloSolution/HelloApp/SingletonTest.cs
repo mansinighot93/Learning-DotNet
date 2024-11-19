a3540f8ac9a06eb74dc024f5185be0dc97820dec
mansinighot93 <mansinighot93@gmail.com>
2024-11-19 16:36:14 +0530

    singleton pattern

--- HelloSolution/HelloApp/SingletonTest.cs
+++ HelloSolution/HelloApp/SingletonTest.cs
@@ -9,7 +9,17 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace HelloApp
-{
+{//Singleton pattern
+ //allow only one instance of a class to be created
+ //1.define private constructor:
+ //private static class_name _ref = null;
+
+    //2.keep slef refernce as satatic variable to class
+    //3.create instance of class verifying refernce nullability with the help of static method
+    //public static class_name getmanager(){ if(_ref == null)
+    //_ref = new class_name();return _ref;
+    //else{
+    //return _ref;}}
     public class SingletonTest
     {
         static void Main(string[] args)
@@ -23,8 +33,12 @@ namespace HelloApp
             Product product2 = new Product(2, "Gerbera", "Weeding Flower", 450, 10);
             Item item1 = new Item(product1, 34);
             Item item2 = new Item(product2, 56);
+            List<Item> cartItem = new List<Item>();
+            DateTime ordDate = DateTime.Now;
+            Customer cust1 = Customer.GetCustomer();
+           
 
-            Cart cart1 = new Cart();
+            
 
         }
     }
