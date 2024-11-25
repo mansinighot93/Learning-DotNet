24f523fe7b59e9acd0282f57aa2c40b9fb4113e7
mansinighot93 <mansinighot93@gmail.com>
2024-11-25 12:35:36 +0530

    event

--- HelloSolution/HelloApp/DelegationText.cs
+++ HelloSolution/HelloApp/DelegationText.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Eventhandler;
 namespace HelloApp
 {
     //Define Delegate
-    public  delegate void Handler();
+    //public  delegate void Handler();
+
     //Delegate at like a function pointer
     //Delegate is a wrapper for the function pointer in C#
     //Delegate always match the signature of those function which would be call as a runtime
@@ -35,8 +36,8 @@ namespace HelloApp
             PayProffesionalTax();
 
             //Late Binding
-            Handler operation1 = null;
-            operation1 = new Handler(PayIncomeTax);
+            EventHandler operation1 = null;
+            operation1 = new EventHandler(PayIncomeTax);
             //operation1();
 
             Handler operation2 = null;
---
+++ HelloSolution/HelloApp/Event.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eventhandler
+{
+    
+    public delegate void EventHandler();
+    public class Event
+    {
+        private float balance;
+        public event EventHandler underbalnce;
+        public event EventHandler overbalnce; 
+        public void Monitoring()
+        {
+            if(balance < 5000)
+            {
+                underbalnce();
+            }
+            else if(balance >=25000)
+            {
+                overbalnce();
+            }
+        }
+    }
+}
