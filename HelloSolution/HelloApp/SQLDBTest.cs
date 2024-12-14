27c63c5bd30c1da7277a3fdf4aede83309bad1db
mansinighot93 <mansinighot93@gmail.com>
2024-12-14 19:24:41 +0530

    Updated project, added DAL files, and ignored database files

---
+++ HelloSolution/HelloApp/SQLDBTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using BLL;
+using Catalog;
+
+namespace HelloApp
+{
+    public class SQLDBTest
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Database Connectivity Program");
+            IEnumerable<Product> allProducts = BuissnessManager.GetAllProducts();
+            foreach (Product product in allProducts)
+            {
+                Console.WriteLine("Title = {0} ,Quantity = {1} , Description = {2} ", product.title,product.Qunatity,product.Description);
+            }
+        }
+    }
+}
