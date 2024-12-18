428b84a01c5d337cc5666bc4b709609b342c3b95
mansinighot93 <mansinighot93@gmail.com>
2024-12-18 19:21:29 +0530

    Create AccountController

---
+++ LearningwebApp/Controllers/AccountController.cs
@@ -0,0 +1,36 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using LearningwebApp.Models;
+
+namespace LearningwebApp.Controllers;
+
+public class AccountsController : Controller
+{
+    private readonly ILogger<AccountsController> _logger;
+
+    public AccountsController(ILogger<AccountsController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    public IActionResult Register()
+    {
+        return View();
+    }
+
+    public IActionResult Forgotpassword()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
--- LearningwebApp/Program.cs
+++ LearningwebApp/Program.cs
@@ -22,7 +22,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Products}/{action=Index}/{id?}")
+    pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
 
 
