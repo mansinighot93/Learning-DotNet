cf2ed26d2f7f6463bde51f5e0963a00c5cf0d688
mansinighot93 <mansinighot93@gmail.com>
2025-01-10 14:59:43 +0530

    mvc and emptyweb App using Thread

---
+++ ThreadTask/MVCApp/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using MVCApp.Models;
+
+namespace MVCApp.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        Thread theThread=Thread.CurrentThread;
+        Console.WriteLine("/default: " + theThread.ManagedThreadId);
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        Thread theThread=Thread.CurrentThread;
+        Console.WriteLine("/default: " + theThread.ManagedThreadId);
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
