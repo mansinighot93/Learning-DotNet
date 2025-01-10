df247b19badc737e27050384d1d69a5cb70e633d
mansinighot93 <mansinighot93@gmail.com>
2025-01-10 13:13:43 +0530

    Threading Task

---
+++ ThreadTask/SimpleTaskApp/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SimpleTaskApp
+{
+
+    //Single Threaded Application
+
+    class Program
+    {
+        //non blocking call
+        public static  async  Task  StoreData(){
+            // defining callback function and invoking callback function
+            // using internal thread pool
+              await Task.Run(()=>{
+                    Thread theTread=Thread.CurrentThread;
+                    Console.WriteLine("Main fn Thread =" + theTread.ManagedThreadId);
+                    Console.WriteLine("storing data to JSON file");
+            });
+        }
+
+        public static  async  Task  GetRemoteData(){
+             await Task.Run(()=>{
+                Thread theTread=Thread.CurrentThread;
+                Console.WriteLine("Main fn Thread =" + theTread.ManagedThreadId);
+                Console.WriteLine("getting  data from external world");
+            });
+        }
+
+        static void Main(string[] args)
+        {
+            //primary Thread:
+            // is to invoke main entrypoint function
+
+            Thread theTread=Thread.CurrentThread;
+            Console.WriteLine("Main fn Thread =" + theTread.ManagedThreadId);
+            //Invoke other reusable function
+           /* StoreData();
+            GetRemoteData();
+            */
+            StoreData();
+            GetRemoteData();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
