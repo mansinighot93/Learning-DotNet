7ae4181ed2b2ab755034b3e564f20c05b5c594aa
mansinighot93 <mansinighot93@gmail.com>
2025-01-14 20:45:41 +0530

    Custom middleware routing with extension multiple methods

---
+++ ThreadTask/MiddleWareRoute/Program.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using MiddleWareRoute;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+
+app.UseFirstMiddleware();
+app.UseSecondMiddleware();
+app.UseLastMiddleware();
+//app.MapGet("/", () => "Hello World!");
+
+//app.UseWelcomePage();   //inbuilt middleware
+//app.UseStaticFiles();   ///inbuilt middleware
+
+app.Run();
+
