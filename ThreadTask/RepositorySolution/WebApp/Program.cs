d66bd74508b86b69e28468d5a34072e7b32c491c
mansinighot93 <mansinighot93@gmail.com>
2025-01-21 19:08:19 +0530

    Dependency Injection with flowers and Fruits

--- ThreadTask/RepositorySolution/WebApp/Program.cs
+++ ThreadTask/RepositorySolution/WebApp/Program.cs
@@ -1,26 +1,40 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Core.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+using Core.Repositories.Interfaces;
+using Core.Services;
+using Core.Services.Interfaces;
 
-namespace WebApp
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<IFlowerRepository,FlowerRepository>();
+builder.Services.AddSingleton<IFruitRepository,FruitRepository>();
+builder.Services.AddSingleton<IFinancialsService,FinancialsService>();
+builder.Services.AddSingleton<IFlowerService,FlowerService>();
+builder.Services.AddSingleton<IFruitService,FruitService>();
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+
+app.Run();
