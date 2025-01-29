d9dbf1b5f608e0cb89b178668236fb9852177cf1
mansinighot93 <mansinighot93@gmail.com>
2025-01-29 13:26:38 +0530

    Modified changes

--- ConfigSetting_Session/SessionManagement/obj/Debug/net9.0/SessionManagement.AssemblyInfo.cs
+++ ConfigSetting_Session/SessionManagement/obj/Debug/net9.0/SessionManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SessionManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8edf2f86c4745c002f12262ccff57aa6d38c0e7c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e80ca8fb7bc05a16c8a9e9cb538485aa1095da52")]
 [assembly: System.Reflection.AssemblyProductAttribute("SessionManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SessionManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
