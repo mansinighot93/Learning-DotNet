e271e06ba77b3f3c35b547bc031f56393bab5220
mansinighot93 <mansinighot93@gmail.com>
2025-02-14 16:02:08 +0530

    Structure of JWTAuthentication

---
+++ JWTAuthentication/Controllers/UsersController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWTAuthentication.Controllers
+{
+    [ApiController]
+    public class UsersController : Controller
+    {
+        public UsersController()
+        {
+
+        }
+        
+    }
+}
